cee4e32d7a7a93a89a02a113ce311cec91ce56a3
FedeManzano <154226686+FedeManzano@users.noreply.github.com>
2025-11-04 22:47:15 -0300

    feat: lógica del sidebar-drop avanzada. Evento click con la función toogle.

--- src/modulos/SidebarDrop.js
+++ src/modulos/SidebarDrop.js
@@ -4,19 +4,19 @@ import ERR from "./Errores"
 (function() {
 
 
-    let elements = {
-        obj: null,
-        state: false,
-        id: ""
-    }
+   let elements = []
+
+
 
     const cantElements = $(".bs-sidebar-title").length
-
+    const conf = {
+        idSidebar : "SinID",
+        idNav: "SinID"
+    }
 
 
     const Init = ({idNav = "SinID", idSidebar = "SinID"}) => {
-        console.log(idNav + " " + idSidebar)
-
+    
         if(!ERR.id.validacion.test(idNav))
         {
             console.error(ERR.id.mensaje)
@@ -28,6 +28,43 @@ import ERR from "./Errores"
             console.error(ERR.id.mensaje)
             return
         }
+
+        conf.idNav = idNav
+        conf.idSidebar = idSidebar
+
+        $(idSidebar + " .bs-sidebar-title").each((index, e) => {
+            let ele = {
+                obj: e,
+                state: false,
+                target: $(e).data("target")
+            }
+            elements[index] = ele
+        }) 
+        EventClick()
+    }
+
+    const EventClick = () => {
+        $(conf.idSidebar + " .bs-sidebar-title").on("click","*", (e) => ToogleLabel(e.target))
+        $(conf.idSidebar + " .bs-sidebar-title").on("click", (e) => Toogle(e.target))
+    }
+
+    const ToogleLabel = (element) => Toogle($(element).parent())
+
+    const Toogle = (targetElement) => {
+        elements.forEach((ele) =>{
+            if($(targetElement).data("target") === $(ele.obj).data("target"))
+            {
+                if(!ele.state)
+                {
+                    $(ele.target).slideDown(150)
+                }
+                else 
+                {
+                    $(ele.target).slideUp(150)
+                }
+                ele.state = !ele.state
+            }
+        })
     }
 
 
