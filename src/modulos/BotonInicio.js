3d3b609c495a25cb9fcc3daa9a469731c1449a90
FedeManzano <154226686+FedeManzano@users.noreply.github.com>
2025-11-05 21:07:18 -0300

    feat: botón inicio documentado en el código JS.

--- src/modulos/BotonInicio.js
+++ src/modulos/BotonInicio.js
@@ -1,35 +1,65 @@
 import $ from 'jquery'
 
+/**
+ * Botoón que permite volver al inició de la 
+ * página (scroll top = 0) desde cualquier lugar 
+ * hacia el inicio.
+ */
 (function(){
+
+    const conf = {
+        show_hide: 200, // Tiempo en tarda en aparecer y desaparecer
+        scroll: 300, // Tiempo que tarda en llevar el scroll a 0
+        limit: 100 // A partir de que nivel del scroll se mostrará el botón
+    }
+
+    // Objeto de Jquery que va a contener 
+    // el botón junto con los estilos asociados
     var boton = null;
 
 
+    // Elmina todos los eventos asociados
+    // Permite limpiar el buffer de eventos
     const destroy = () => {
         $(boton).off()
     }
+
+    /**
+     * Función que permite inicializar el módulo del botón
+     */
     var inicializar = ()=> {
+
+        // El elemento es un div con la clase .boton-inicio
         boton = $("<div class='boton-inicio'></div>")
-        $(boton).hide()
+        $(boton).hide() // Oculta el elemnto
+
+        // Se carga el botón al body
         $("body").append(boton)
     }
 
     var scroll = () => {
-        $(window).scroll(function(){
-            if($(this).scrollTop() > 100){
-                $(boton).show(200) 
+
+        // Evento scroll del evento 
+        // de la ventana cuando supera 
+        // a 100px se muestra el botón
+        $(window).on("scroll", () => {
+            if($(this).scrollTop() > conf.limit){
+                $(boton).show(conf.show_hide) 
             }else {
-                $(boton).hide(200)
+                $(boton).hide(conf.show_hide)
             }
         })
     }
 
-
+    /**
+     * Permite que cuando se hace click al botón
+     * llevar el scroll de la venta  a 0
+     */
     var activar = () => {
-        $(boton).click(function(){
-     
+        $(boton).on("click", () => {
             $("html, body").animate({
                 scrollTop: 0
-            }, 300)
+            }, conf.scroll)
          
         })
     }
@@ -42,7 +72,10 @@ import $ from 'jquery'
 
         destroy: () => destroy()
     }
+    // Se carga el elemento en la ventana
     window.BotonInicio = BotonInicio
 })()
 
+// Exporta el elemento 
+// Se lo inicializa en el archivo app.js
 export default BotonInicio
