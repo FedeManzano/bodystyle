f41be6c85cd0eb0b61527cb6eec7642be8feba93
FedeManzano <154226686+FedeManzano@users.noreply.github.com>
2025-11-23 23:59:08 -0300

    fix: explicit include for css loader in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,13 @@ module.exports = {
     filename: 'bodystyle.js',
     path: path.join(__dirname, 'dist/js'),
   },
+  resolve: {
+    extensions: ['.js', '.css']
+  },
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.css$/, include: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'node_modules')], use: ["style-loader", "css-loader"] },
     ]
   },
   mode: "development"
